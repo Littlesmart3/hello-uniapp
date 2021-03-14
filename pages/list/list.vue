53e1e8b93ea2028ac7b3de367c65fcd6f61d27a5
Littlesmart3 <798347235@qq,com>
2021-03-14 20:13:49 +0800

    day23

--- pages/list/list.vue
+++ pages/list/list.vue
@@ -1,6 +1,9 @@
 <template>
 	<view>
 		<button @click="get">发送get请求</button>
+		<button type="primary" @click="setStorage">存储数据</button>
+		<button type="primary" @click="getStorage">获取数据</button>
+		<button type="primary" @click="removeId">获取数据</button>
 		<view>这是列表页</view>
 		<view class="box-item" v-for="item in list">
 			{{item}}
@@ -10,41 +13,70 @@
 </template>
 
 <script>
-	export default{
-		data(){
-			return{
-				list:['前端','JAVA','Python','javascript','大数据','前端','JAVA','Python','javascript','大数据']
+	export default {
+		data() {
+			return {
+				list: ['前端', 'JAVA', 'Python', 'javascript', '大数据', '前端', 'JAVA', 'Python', 'javascript', '大数据']
 			}
 		},
-		onPullDownRefresh(){
+		onPullDownRefresh() {
 			console.log("触发了下拉刷新！")
 			setTimeout(() => {
-				this.list=['JAVA','Python','javascript','大数据','前端']
+				this.list = ['JAVA', 'Python', 'javascript', '大数据', '前端']
 				uni.stopPullDownRefresh()
-			},2000)
+			}, 2000)
 		},
-		onReachBottom(){
+		onReachBottom() {
 			console.log("页面触底了")
-			this.list=[...this.list,...['JAVA','Python','javascript','大数据','前端']]
+			this.list = [...this.list, ...['JAVA', 'Python', 'javascript', '大数据', '前端']]
 		},
-		methods:{
+		methods: {
 			pullDown() {
 				uni.startPullDownRefresh()
 			},
 			get() {
 				uni.request({
-					url:"http://localhost:8082/api/getlunbo",
-					success(res){
+					url: "http://localhost:8082/api/getlunbo",
+					success(res) {
 						console.log(res)
 					}
 				})
+			},
+			setStorage() {
+				// uni.setStorage({
+				// 	key: "id",
+				// 	data: 80,
+				// 	success() {
+				// 		console.log("存储成功")
+				// 	}
+				// })
+				uni.setStorageSync('id', 100) 
+			},
+			getStorage() {
+				// uni.getStorage({
+				// 	key: 'id',
+				// 	success(res) {
+				// 		console.log('获取成功', res)
+				// 	}
+				// })
+				const res = uni.getStorageSync('id')
+				console.log(res)
+			},
+			removeId() {
+				// uni.removeStorage({
+				// 	key: 'id',
+				// 	success() {
+				// 		console.log('删除成功')
+				// 	}
+				// })
+				uni.removeStorageSync('id')
 			}
 		}
 	}
 </script>
 
 <style>
-	.box-item{
+	.box-item {
 		height: 100px;
 		line-height: 100px;
 	}
--- pages/message/message.vue
+++ pages/message/message.vue
@@ -1,11 +1,61 @@
 <template>
 	<view>
-		hello-uni
+		<button type="primary" @click="chooseImg">上传图片</button>
+		<image v-for='item in imgArr' :src="item" @click="previewImg(item)"></image>
+		<!-- #ifdef  H5 -->
+		<view>我希望在H5中打印</view>
+		<!-- #endif -->
+		<!-- #ifdef MP-WEIXIN -->
+		<view>我希望在微信小程序中使用</view>
+		<!-- #endif -->
 	</view>
 </template>
 
 <script>
+	export default {
+		data() {
+			return {
+				imgArr: []
+			}
+		},
+		methods: {
+			chooseImg() {
+				uni.chooseImage({
+					count: 5,
+					success: res => {
+						this.imgArr = res.tempFilePaths
+					}
+				})
+			},
+			previewImg(current) {
+				uni.previewImage({
+					current,
+					urls: this.imgArr,
+					loop: true,
+					indicator: 'number'
+				})
+			},
+			onLoad() {
+				// #ifdef H5
+				console.log('我希望在H5中打印')
+				// #endif
+				// #ifdef MP-WEIXIN
+				console.log('我希望在微信小程序中使用')
+				// #endif
+			}
+		}
+	}
 </script>
 
 <style>
+	/* #ifdef H5 */
+	view{
+		color: hotpink;
+	}
+	/* #endif */
+	/* #ifdef MP-WEIXIN */
+	view{
+		color: hotpink;
+	}
+	/* #endif */
 </style>
